4481ebde339724ca2904758d1c9721d51872e4b3
Prashant Tripathi <prashanttripathi@in-prashanttripathi.local>
2016-10-06 16:18:04 +0530

    test added to service side.

---
+++ kg_services/public/javascripts/calculator.js
@@ -0,0 +1,19 @@
+var calculator = {};
+exports.calculator = calculator;
+
+calculator.add = function(digit_1,digit_2){
+  return digit_1 + digit_2;
+}
+
+
+calculator.subtract = function(digit_1,digit_2){
+  return digit_1 - digit_2;
+}
+
+calculator.multiply = function(digit_1,digit_2){
+  return digit_1 * digit_2;
+}
+
+calculator.divide = function(digit_1,digit_2){
+  return digit_1 / digit_2;
+}
